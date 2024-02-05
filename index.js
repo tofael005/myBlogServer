668b22ee06b8b611de156f0105e73367a63bc65c
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-02-05 21:41:02 +0600

    new setup server

--- index.js
+++ index.js
@@ -1,52 +1,16 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-
+const express = require('express')
 const app = express()
-app.use(cors())
-app.use(express.json())
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://tofael6469:5uPH6lDXxh8vzJx3@cluster0.tsm0iem.mongodb.net/?retryWrites=true&w=majority";
+app.get('/youtube', (req, res) => {
+  res.send("Subscribe my channel")
+})
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    
-    const blogsCollection = client.db("allmyblogs").collection.blogsView;
-
-
-     // College data get 
-     app.get("/allBlogs", async (req, res) =>{
-        const allBlogs = await blogsCollection.find().toArray()
-        res.send(allBlogs)
-    })
-
-
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-app.listen(3001, () => {
-    console.log("Server is running")
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
