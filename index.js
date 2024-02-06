c3720f3a71e3e6c9c3811fa6b75472ea0e4f1f0c
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-02-06 10:46:31 +0600

    json added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
+app.use(cors())
 const port = 3000
+const data = require("./data.json");
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -11,6 +14,10 @@ app.get('/youtube', (req, res) => {
   res.send("Subscribe my channel")
 })
 
+
+app.get('/mealbd', (req, res) => {
+  res.send({data})
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
