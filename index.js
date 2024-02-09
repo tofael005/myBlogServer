a53c95fc930bd3d3bc709aa191c26a8bd9bbd462
Tofael Al Hasan <arnobtuhin.j@gmail.com>
2024-02-09 23:34:28 +0600

    app get func

--- index.js
+++ index.js
@@ -28,6 +28,12 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const myBlogCollection = client.db("My-blog").collection("blogs")
+
+
+    app.get("/blogs", async (req, res) => {
+      const result = await myBlogCollection.find().toArray();
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
